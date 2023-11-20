5ef439bf61019012dd13cd0b023a3b5d6fb9e698
redhawk2002 <kalitasandeepan@gmail.com>
2023-11-20 22:51:30 +0530

    added the controller routes and updated the server

--- server.js
+++ server.js
@@ -1,6 +1,11 @@
 const express = require("express");
 const app = express();
+const bodyParser = require("body-parser");
+require("dotenv").config({ path: "./config/.env" });
+const user = require("./routes/user");
+app.use(bodyParser.json());
 
+app.use("/api/v1", user);
 app.listen(process.env.PORT, () => {
-  console.log(`server is listening in PORT ${lprocess.env.PORT}`);
+  console.log(`server is listening in PORT ${process.env.PORT}`);
 });
